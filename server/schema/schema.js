ae991fc42a5f652e929cb3821354a9d2fd8454fe
mulugeta-89 <mulugetahail89@gmail.com>
2023-12-22 15:34:49 +0300

    project mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -101,6 +101,13 @@ const mutation = new GraphQLObjectType({
                 return project.save()
             }
         },
+        deleteProject: {
+            type: ProjectType,
+            args: { id: { type: GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
+                return Project.findByIdAndDelete(args.id)
+            }
+        },
         addClient: {
             type: ClientType,
             args: {
